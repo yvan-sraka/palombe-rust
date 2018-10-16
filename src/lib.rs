b05f0ae9c22947772e404d31d0bf5e4ba6dc2aef
Yvan Sraka <yvan@sraka.pw>
2018-10-16 11:16:32 +0200

    v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ extern crate libc;
 use std::io::prelude::*;
 
 pub fn send(name: String, value: String) {
-    let path = format!("/tmp/palombe/{}", name);
-    std::fs::create_dir_all("/tmp/palombe/")
-        .expect("Error: couldn't create the /tmp/palombe/ folder");
+    let prefix = "/tmp/palombe/";
+    let path = format!("{}{}", prefix, name);
+    std::fs::create_dir_all(prefix)
+        .expect(&format!("Error: couldn't create the {} folder", prefix));
     let filename = std::ffi::CString::new(path.clone()).unwrap();
     unsafe { libc::mkfifo(filename.as_ptr(), 0o644); }
     std::thread::spawn(move || {
@@ -16,7 +17,8 @@ pub fn send(name: String, value: String) {
 }
 
 pub fn receive(name: String) -> String {
-    let path = format!("/tmp/palombe/{}", name);
+    let prefix = "/tmp/palombe/";
+    let path = format!("{}{}", prefix, name);
     let file = std::fs::File::open(path.clone())
         .expect(&format!("Error: couldn't open: {}", path));
     let mut reader = std::io::BufReader::new(file);
