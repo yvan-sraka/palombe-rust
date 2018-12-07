0c067392e521a77547e3715f924f4308335d8bec
Yvan Sraka <yvan.sraka@gmail.com>
2018-12-07 11:10:05 +0100

    % rustfmt src/main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,18 @@ fn __mkfifo(name: String) -> String {
     std::fs::create_dir_all(prefix)
         .expect(&format!("Error: couldn't create the folder {}", prefix));
     let filename = std::ffi::CString::new(path.clone()).unwrap();
-    unsafe { libc::mkfifo(filename.as_ptr(), 0o600); }
+    unsafe {
+        libc::mkfifo(filename.as_ptr(), 0o600);
+    }
     return path;
 }
 
 #[no_mangle]
 pub extern "C" fn send(name: String, value: String) {
     let path = __mkfifo(name);
-    let mut file = std::fs::OpenOptions::new().write(true).open(path)
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .open(path)
         .expect("Error: couldn't open the named pipe");
     file.write_all(value.as_bytes())
         .expect("Error: couldn't write the named pipe");
@@ -23,12 +27,12 @@ pub extern "C" fn send(name: String, value: String) {
 #[no_mangle]
 pub extern "C" fn receive(name: String) -> String {
     let path = __mkfifo(name);
-    let file = std::fs::File::open(path.clone())
-        .expect(&format!("Error: couldn't open: {}", path));
+    let file = std::fs::File::open(path.clone()).expect(&format!("Error: couldn't open: {}", path));
     let mut reader = std::io::BufReader::new(file);
     let mut buffer = String::new();
     loop {
-        let len = reader.read_line(&mut buffer)
+        let len = reader
+            .read_line(&mut buffer)
             .expect("Error: couldn't read the input file");
         if len == 0 {
             std::fs::remove_file(&path)
