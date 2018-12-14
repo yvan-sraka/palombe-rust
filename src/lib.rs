5f7b804788ea9f9be1cd32407708e5c20b828995
Yvan Sraka <yvan.sraka@gmail.com>
2018-12-14 10:20:22 +0100

    Fix lints given by Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 extern crate libc;
 use std::io::prelude::*;
 
-fn __mkfifo(name: String) -> String {
+fn __mkfifo(name: &str) -> String {
     let prefix = "/tmp/palombe/";
     let path = format!("{}{}", prefix, name);
     std::fs::create_dir_all(prefix)
-        .expect(&format!("Error: couldn't create the folder {}", prefix));
+        .unwrap_or_else(|_| panic!("Error: couldn't create the folder {}", prefix));
     let filename = std::ffi::CString::new(path.clone()).unwrap();
     unsafe {
         libc::mkfifo(filename.as_ptr(), 0o600);
     }
-    return path;
+    path
 }
 
 #[no_mangle]
 pub extern "C" fn send(name: String, value: String) {
-    let path = __mkfifo(name);
+    let path = __mkfifo(&name);
     let mut file = std::fs::OpenOptions::new()
         .write(true)
         .open(path)
@@ -26,8 +26,9 @@ pub extern "C" fn send(name: String, value: String) {
 
 #[no_mangle]
 pub extern "C" fn receive(name: String) -> String {
-    let path = __mkfifo(name);
-    let file = std::fs::File::open(path.clone()).expect(&format!("Error: couldn't open: {}", path));
+    let path = __mkfifo(&name);
+    let file = std::fs::File::open(path.clone())
+        .unwrap_or_else(|_| panic!("Error: couldn't open: {}", path));
     let mut reader = std::io::BufReader::new(file);
     let mut buffer = String::new();
     loop {
@@ -36,7 +37,7 @@ pub extern "C" fn receive(name: String) -> String {
             .expect("Error: couldn't read the input file");
         if len == 0 {
             std::fs::remove_file(&path)
-                .expect(&format!("Error: couldn't remove the file {}", path));
+                .unwrap_or_else(|_| panic!("Error: couldn't remove the file {}", path));
             return buffer;
         }
     }
